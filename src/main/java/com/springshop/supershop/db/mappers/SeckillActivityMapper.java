23b66870842cbfcdb02e499ad39ab0f9ac2bb6a8
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-09 01:29:23 -0600

    updated test

--- src/main/java/com/springshop/supershop/db/mappers/SeckillActivityMapper.java
+++ src/main/java/com/springshop/supershop/db/mappers/SeckillActivityMapper.java
@@ -2,6 +2,8 @@ package com.springshop.supershop.db.mappers;
 
 import com.springshop.supershop.db.po.SeckillActivity;
 
+import java.util.List;
+
 public interface SeckillActivityMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +16,6 @@ public interface SeckillActivityMapper {
     int updateByPrimaryKeySelective(SeckillActivity record);
 
     int updateByPrimaryKey(SeckillActivity record);
+
+    List<SeckillActivity> querySeckillActivitysByStatus(int activityStatus);
 }
