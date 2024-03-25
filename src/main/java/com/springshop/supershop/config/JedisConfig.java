7c0cd47c4837fd826af5a02a89a12e6ac6d74805
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-03-24 21:00:14 -0500

    Update Redis configuration integrated in JedisConfig.java, and its pom.xml dependency.

---
+++ src/main/java/com/springshop/supershop/config/JedisConfig.java
@@ -0,0 +1,57 @@
+package com.springshop.supershop.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.text.MessageFormat;
+
+@Configuration
+public class JedisConfig extends CachingConfigurerSupport {
+
+    private Logger logger = LoggerFactory.getLogger(JedisConfig.class);
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.jedis.pool.max-active}")
+    private int maxActive;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.min-idle}")
+    private int minIdle;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Bean
+    public JedisPool redisPoolFactory() {
+        JedisPoolConfig config = new JedisPoolConfig();
+
+        config.setMaxIdle(this.maxIdle);
+        config.setMinIdle(this.minIdle);
+        config.setMaxWaitMillis(this.maxWaitMillis);
+        config.setMaxTotal(this.maxActive);
+
+        JedisPool pool = new JedisPool(config, host, port, timeout, null);
+
+        logger.info("JedisPool injected successfully!");
+        logger.info(MessageFormat.format("Redis address: {0}:{1}", host, port));
+
+        return pool;
+    }
+
+}
